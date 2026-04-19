2ae4e8652e3dea32a704f80b31e82bb2e5bb404a
jmirandal1994 <jmiraandal@gmail.com>
2026-04-19 18:39:54 -0400

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -15,9 +15,9 @@ export async function GET(request: NextRequest) {
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
-          get(name) { return cookieStore.get(name)?.value },
-          set(name, value, options) { cookieStore.set({ name, value, ...options }) },
-          remove(name, options) { cookieStore.set({ name, value: '', ...options }) },
+          get(name: string) { return cookieStore.get(name)?.value },
+          set(name: string, value: string, options: any) { cookieStore.set({ name, value, ...options }) },
+          remove(name: string, options: any) { cookieStore.set({ name, value: '', ...options }) },
         },
       }
     )
